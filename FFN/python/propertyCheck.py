5acbb55ea5780f26129f4a656614538e27c4d97c
Moumita Das <41421406+DMoumita@users.noreply.github.com>
2021-06-21 15:49:01 +0100

    Update propertyCheck.py

--- FFN/python/propertyCheck.py
+++ FFN/python/propertyCheck.py
@@ -2,7 +2,7 @@ import numpy as np
 import re
 import subprocess
 def findObjectiveFuncionType(prop):
-    print("Mou",prop)
+    
     if (prop == "1" or prop == "2" or prop == "6" or prop == "8" or prop == "10"):
         target= 0
         objType = 0 # 0 for maximize
@@ -12,7 +12,7 @@ def findObjectiveFuncionType(prop):
     if (prop == "5") :
         target= 4
         objType = 0 # 1 for minimize
-    if (prop == "7" or prop == "9"):
+    if (prop == "7" ):
         target= 3
         objType = 1 # 1 for minimize
     if (prop == "9"):
@@ -95,42 +95,5 @@ def checkAndSegregateSamplesForMinimum(posSample,negSample, smple,oldPos,targetN
             posSample.remove(posSample[0])
             posSample.append(oldPos[0])
 
-def loadInputRange(prop):          
-    inRange=[]   
-    lower=[]      
-    upper=[]       
-    if (prop == "1") :        
-        lower=[55947.691,-3.141592,-3.141592,1145,0]     
-        upper=[60760,3.141592,3.141592, 1200, 60]      
-    if (prop == "2") :       
-        lower=[55947.691,-3.141592,-3.141592,1145,0]    
-        upper=[60760,3.141592,3.141592, 1200, 60]    
-    if (prop == "3") :  
-        lower=[1500,-0.06,3.10,980,960]     
-        upper=[1800,0.06,3.141592,1200,1200]        
-    if (prop == "4") :  
-        lower=[1500,-0.06,0,1000,700]         
-        upper=[1800,0.06,0,1200,800]    
-    if (prop == "5") : 
-        lower=[250,0.2,-3.1415926,100,0]     
-        upper=[400,0.4,-3.1415926+0.005,400,400]       
-    if (prop == "6") : 
-        lower=[12000,-3.141592,-3.141592,100,0]   
-        upper=[62000,-0.7,-3.141592+0.005,200,1200]        
-    if (prop == "7") :  
-        lower=[0,-3.141592,-3.141592,100,0]    
-        upper=[60760,3.141592,3.141592,1200,1200]      
-    if (prop == "8") :  
-        lower=[0,-3.141592,-0.1,600,600]     
-        upper=[60760,-3.141592*0.75,0.1,1200,1200]     
-    if (prop == "9") :    
-        lower=[2000,-0.4,-3.141592,100,0]    
-        upper=[7000,-0.14,-3.141592+0.01,150,150]        
-    if (prop == "10") :     
-        lower=[36000,0.7,-3.141592,900,600]       
-        upper=[60760,3.141592,-3.141592+0.01,1200,1200]       
-    inRange.append(lower)  
-    inRange.append(upper)   
-    tMat=np.transpose(inRange)     
-    return tMat      
+
 
