053afb6efcdebd40964852e5cb544f2b74c0854d
Rajarshi <rajarshi.ray@iacs.res.in>
2021-06-21 10:46:43 -0400

    updated install_tool.sh

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -1,6 +1,18 @@
-apt install python3-pip
+TOOL_NAME=FFN
+VERSION_STRING=v1
 
-#apt-get install protobuf-compiler libprotoc-dev
+# check arguments
+if [ "$1" != ${VERSION_STRING} ]; then
+	echo "Expected first argument (version string) '$VERSION_STRING', got '$1'"
+	exit 1
+fi
+
+echo "Installing $TOOL_NAME"
+DIR=$(dirname $(dirname $(realpath $0)))
+
+apt-get update &&
+apt-get install -y python3 python3-pip &&
+
 pip3 install "onnx==1.8.0"
 pip3 install numpy
 pip3 install z3-solver
