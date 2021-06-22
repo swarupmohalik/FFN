d180897c219f45de27c4d54b99ff71aab8791c89
Moumita Das <41421406+DMoumita@users.noreply.github.com>
2021-06-22 15:00:02 +0100

    Update run_instance.sh

--- vnncomp_scripts/run_instance.sh
+++ vnncomp_scripts/run_instance.sh
@@ -27,7 +27,7 @@ timeoutFlag=0
 timeout=0
 while(true)
 do
-  python FFN/python/sampleEval.py inputRangeFile $propNum propSpecFile  $3 2</dev/null>A
+  python vnncomp2021/FFN/python/sampleEval.py inputRangeFile $propNum propSpecFile  $3 2</dev/null>A
   tm=`grep "Time" A |cut -d " " -f4`
   totTm=`echo $tm + $totTm | bc`
   grep "Number of samples ::" A 1</dev/null
