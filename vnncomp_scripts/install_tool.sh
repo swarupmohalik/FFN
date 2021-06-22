b30c380fa12abe44e1dae590d0cdb3bb8b6f4d66
Moumita Das <41421406+DMoumita@users.noreply.github.com>
2021-06-22 15:49:10 +0100

    Update install_tool.sh

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -17,6 +17,6 @@ pip3 install "onnx==1.8.0"
 pip3 install numpy
 pip3 install z3-solver
 pip3 install onnxruntime
-#pip3 install torch
-#pip3 install future
+pip3 install torch
+pip3 install future
 
