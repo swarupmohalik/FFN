ee3c29bafefeb30dbb7766e59b27a9f61c9ef49c
Moumita Das <41421406+DMoumita@users.noreply.github.com>
2021-06-24 23:43:19 +0100

    Update run_instance.sh

--- vnncomp_scripts/run_instance.sh
+++ vnncomp_scripts/run_instance.sh
@@ -7,7 +7,7 @@ echo "vnnlib file path:: " $4
 echo "result file path:: " $5
 echo "timeout:: " $6
 
-
+START=$(date +%s.%N)
 propFile=$4
 
 propNum=`echo $propFile|cut -d "_" -f2|cut -d "." -f1`
@@ -25,7 +25,6 @@ cat $propFile|tail +$lno>propSpecFile
 
 
 totTm=0.0
-totSmpl=0
 timeoutFlag=0
 timeout=0
 while(true)
@@ -36,25 +35,14 @@ do
      echo error >$5
      exit 0
   fi
-
-  tm=`grep "Time" A |cut -d " " -f4`
-  totTm=`echo $tm + $totTm | bc`
-  grep "Number of samples ::" A 1</dev/null
-  if [ $? -eq 0 ]    
-  then     
-    smpl=`grep "Number of samples ::" A |cut -d " " -f6`
-    totSmpl=$(($smpl + $totSmpl))
-  else
-    smpl=0
-  fi
+  END=$(date +%s.%N)
+  totTm=`echo $END - $START | bc`
   grep "Adversarial !" A  1</dev/null      
   if [ $? -eq 0 ]   
   then     
-    echo ADVERSARIAL FOUND :: Time :: $totTm :: NUMSAMPLE :: $totSmpl>R
+    echo ADVERSARIAL FOUND :: Time :: $totTm>R
     echo ADVERSARIAL >>R1
     break
-  else
-    totSmpl=$(($totSmpl+1500))
   fi       
   timeout=`echo "$totTm > $6"|bc`
   if [ $timeout -eq 1 ]
@@ -64,7 +52,6 @@ do
     then
         propFile="FFN/FFN/prop_6b.vnnlib"
 	totTm=0.0
-	totSmpl=0
 	timeoutFlag=0
 	timeout=0
         continue
