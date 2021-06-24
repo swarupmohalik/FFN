1dead72d1aea016e94b0d83fab1b41752836f526
Moumita Das <41421406+DMoumita@users.noreply.github.com>
2021-06-24 11:57:35 +0100

    Update run_instance.sh

--- vnncomp_scripts/run_instance.sh
+++ vnncomp_scripts/run_instance.sh
@@ -17,9 +17,12 @@ then
 propFile="FFN/FFN/prop_6a.vnnlib"
 fi
 
-grep assert $propFile|grep -v "Y" | head -10|cut -d " " -f4|cut -d ")" -f1>inputRangeFile
-lno=`grep -n "assert" $propFile|head -n 11|tail +11|cut -d : -f1`
+grep assert $propFile|grep -v "Y" |grep "X"|cut -d " " -f4|cut -d ")" -f1>inputRangeFile
+numInput=`cat inputRangeFile|wc -l`
+numInput=$(($numInput+1))
+lno=`grep -n "assert" $propFile|head -$numInput|tail +$numInput|cut -d : -f1`
 cat $propFile|tail +$lno>propSpecFile
+
 
 totTm=0.0
 totSmpl=0
@@ -28,6 +31,12 @@ timeout=0
 while(true)
 do
   python3 FFN/FFN/python/sampleEval.py inputRangeFile $propNum propSpecFile  $3 2</dev/null>A
+  if [ $? != 0 ]
+  then
+     echo error >$5
+     exit 0
+  fi
+
   tm=`grep "Time" A |cut -d " " -f4`
   totTm=`echo $tm + $totTm | bc`
   grep "Number of samples ::" A 1</dev/null
@@ -67,22 +76,14 @@ do
 done    
 if [ $timeoutFlag  -eq 1 ]   
 then
-  echo TIMEOUT  FOUND :: Time :: $tm :: NUMSAMPLE :: $s1>R
   echo TIMEOUT >>R1
-  ns=0
-  nt=0.0
-else
-  nt=`grep "Time" R |cut -d " " -f6`
-  ns=`grep "NUMSAMPLE" R |cut -d " " -f10`
 fi
 grep "ADVERSARIAL" R1 1</dev/null      
 if [ $? -eq 0 ]   
 then     
-#echo $4  ::  $3 ::  ADVERSARIAL FOUND ::  Time :: $nt :: NUMSAMPLE :: $ns >>Report_all
-echo violated >$5
+    echo violated >$5
 else
-#echo $4  ::  $3 ::  TIMEOUT >>Report_all
-echo timeout >$5
+    echo timeout >$5
 fi       
 rm -f A R R1 test.smt inputRangeFile propSpecFile
 
